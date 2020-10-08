b1e6af8113a87c8d33fe84f12ed7d6c86a6e6be7
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-10-08 12:42:14 -0700

    Problem 2 Test is finished (Hopefully works)

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,3 +1,13 @@
+/*
+    Nam Nguyen
+    October 8th, 2020
+    CSS 143B
+    homework_1
+
+    Sources used:
+    https://www.geeksforgeeks.org/bubble-sort/
+ */
+
 public class Problem2 {
 
     // Do not change signature (function name, parameters)
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -8,33 +8,44 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
+        //assertTrue(false);
         // add your tests here
 
-            int inputs[][] = {
-                    {},
-                    {5},
-                    {8,2},
-                    {9,3,6},
-                    {1,1,1},
-                    {-4,-10,5,20},
-                    {-4,-4,-10,5,20},
-                    {8,30,25,-5,7,90},
-                    {43,-4,8,31,7,10,15}
-            };
-        }
-        int[][] expected = {
+        int inputs[][] = {
                 {},
                 {5},
-                {2,8},
-                {3,6,9},
-                {1,1,1},
-                {-10,-4,5,20},
-                {-10,-4,-4,5,20},
-                {-5,7,8,25,30,90},
-                {-4,7,8,10,15,31,43}
+                {8, 2},
+                {9, 3, 6},
+                {1, 1, 1},
+                {-4, -10, 5, 20},
+                {-4, -4, -10, 5, 20},
+                {8, 30, 25, -5, 7, 90},
+                {43, -4, 8, 31, 7, 10, 15}
         };
 
 
+        int[][] answer = {
+                {},
+                {5},
+                {2, 8},
+                {3, 6, 9},
+                {1, 1, 1},
+                {-10, -4, 5, 20},
+                {-10, -4, -4, 5, 20},
+                {-5, 7, 8, 25, 30, 90},
+                {-4, 7, 8, 10, 15, 31, 43}
+        };
+
+        assertEquals(inputs.length, answer.length);
+
+        for (int i = 0; i < inputs.length; i++) {
+            int[] expected =  answer[i];
+            Problem2.bubbleSort(inputs[i]);
+            int[] actual = inputs[i];
+            assertArrayEquals(expected, actual);
+        }
     }
+}
+
+
 
